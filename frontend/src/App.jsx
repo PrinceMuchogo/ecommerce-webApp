058b30e7dfdcb61b3dc22bc6709454957449f9a4
Prince Muchogo <108458278+PrinceMuchogo@users.noreply.github.com>
2023-06-13 00:38:46 +0200

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 
-import Header from "./components/header";
+import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
